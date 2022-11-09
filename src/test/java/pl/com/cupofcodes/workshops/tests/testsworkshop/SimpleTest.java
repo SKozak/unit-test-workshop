724359cbfe16dad390518a82ad0679583b87d96c
softko <sebastian.kozak@Outlook.com>
2022-11-09 10:55:09 +0100

    initial commit with project source * added simple test

---
+++ src/test/java/pl/com/cupofcodes/workshops/tests/testsworkshop/SimpleTest.java
@@ -0,0 +1,11 @@
+package pl.com.cupofcodes.workshops.tests.testsworkshop;
+
+import org.junit.jupiter.api.Test;
+
+class SimpleTest {
+
+    @Test
+    void dummyTest(){
+        assert  1 == 1;
+    }
+}
